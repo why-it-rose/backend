2ced1f644a969fa0d0c6cf9f2babe3c06467fefb
JooeonLee <cacophony0609@gmail.com>
2026-03-17 20:24:01 +0900

    feat(init): 프로젝트 초기화

---
+++ api-server/src/main/java/com/whyitrose/apiserver/WhyItRoseApplication.java
@@ -0,0 +1,12 @@
+package com.whyitrose.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyItRoseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhyItRoseApplication.class, args);
+    }
+}
