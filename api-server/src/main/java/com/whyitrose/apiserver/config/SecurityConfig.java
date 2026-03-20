44058d66379310139b84620a11cff178f461dbaa
sonnny0625 <andyson0205@gmail.com>
2026-03-20 15:05:44 +0900

    chore: resolve conflict

--- api-server/src/main/java/com/whyitrose/apiserver/config/SecurityConfig.java
+++ api-server/src/main/java/com/whyitrose/apiserver/config/SecurityConfig.java
@@ -19,11 +19,6 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
