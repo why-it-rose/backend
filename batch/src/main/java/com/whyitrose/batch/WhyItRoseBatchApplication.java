3ea03755c7d45408a80558080546d8e17c3e11b7
Sihyun Jang <jjj99nine2@gmail.com>
2026-03-22 09:12:58 +0900

    feat: add stock_prices data store

--- batch/src/main/java/com/whyitrose/batch/WhyItRoseBatchApplication.java
+++ batch/src/main/java/com/whyitrose/batch/WhyItRoseBatchApplication.java
@@ -5,9 +5,11 @@ import com.whyitrose.batch.config.LsOpenApiProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.whyitrose.batch")
+@EnableJpaAuditing
 @EntityScan(basePackages = "com.whyitrose.domain")
 @EnableJpaRepositories(basePackages = "com.whyitrose.domain")
 @EnableConfigurationProperties(LsOpenApiProperties.class)
